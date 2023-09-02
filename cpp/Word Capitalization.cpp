663f28bd9db04e920ab5bc8e494bb4bb27a57494
Cristian Rian <127137723+otoyaa@users.noreply.github.com>
2023-09-02 07:58:50 -0300

    Create Word Capitalization.cpp

---
+++ cpp/Word Capitalization.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    string a;cin>>a;
+    char b=toupper(a[0]);
+    a[0]=b;
+    cout<<a;
+}
